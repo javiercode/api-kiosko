7abc5992c592cbe794569ea042465a41b7076701
javiercode <javier.azote@gmail.com>
2023-06-06 18:57:10 -0400

    correciones sql

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -8,35 +8,22 @@ import { User } from "../entities/User";
 import { Marca } from "../entities/Marca";
 
 export const MysqlDataSource = new DataSource({
-    // "type": "mysql",
-    // "host": process.env.MYSQLDB_HOST,
-    // "port": Number(process.env.MYSQLDB_PORT),
-    // "username": process.env.MYSQLDB_USR,
-    // "password": process.env.MYSQLDB_PSW,
-    // "database": process.env.MYSQLDB,
-    // "synchronize": true,
-    // "logging": false,
-    // "ssl": false,
-    // "extra": {
-    //     ssl: {
-    //         rejectUnauthorized: true,
-    //     },
-    // },
 
     type: 'mysql',
-    host: process.env.MYSQLDB_HOST,
-    port: Number(process.env.MYSQLDB_PORT),
-    username: process.env.MYSQLDB_USR,
-    password: process.env.MYSQLDB_PSW,
-    database: process.env.MYSQLDB,
+    // host: process.env.MYSQLDB_HOST,
+    // port: Number(process.env.MYSQLDB_PORT),
+    // username: process.env.MYSQLDB_USR,
+    // password: process.env.MYSQLDB_PSW,
+    // database: process.env.MYSQLDB,
     synchronize: false,
     logging: false,
-    ssl:false,
-    // extra: {
-    //      ssl: {
-    //          rejectUnauthorized: false,
-    //      },
-    //  },
+    url:'mysql://u33ruzlc3cu969f1owkc:pscale_pw_HczwlazLpizUlT3Hg6V3hNecfUmOVVr4SQV1Qf6w4le@aws.connect.psdb.cloud/kiosko?ssl={"rejectUnauthorized":true}',
+    // ssl:false,
+    extra: {
+         ssl: {
+             rejectUnauthorized: false,
+         },
+     },
     entities: [
         // "./src/entities/*.ts"
         Movimiento,Categoria,Producto,Rol,RolUser,User,Marca
